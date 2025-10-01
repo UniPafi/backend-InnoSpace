eb38b1257a53ee4b8a74f6a9c1fa7483337f8571
Renzo <renzor274@gmail.com>
2025-10-01 08:22:50 -0500

    initial commit

---
+++ src/main/java/com/innospace/platform/InnospacePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.innospace.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnospacePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnospacePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innospace/platform/InnospacePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innospace.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnospacePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
