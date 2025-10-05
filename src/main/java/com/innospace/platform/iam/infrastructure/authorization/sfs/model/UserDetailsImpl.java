11a4c4fff287826e00b78228c9eb8ad8d55711c5
Renzo <u20231d974@upc.edu.pe>
2025-10-04 22:39:05 -0500

    feat: add Spring Security configuration and authentication classes
    Introduces WebSecurityConfiguration for HTTP security setup, BearerAuthorizationRequestFilter for JWT-based authentication, UnauthorizedRequestHandlerEntryPoint for handling unauthorized requests, UserDetailsImpl for custom user details, EmailPasswordAuthenticationTokenBuilder for token creation, and UserDetailsServiceImpl for loading user details from the repository. These additions enable stateless authentication and authorization using JWT tokens.

---
+++ src/main/java/com/innospace/platform/iam/infrastructure/authorization/sfs/model/UserDetailsImpl.java
@@ -0,0 +1,52 @@
+package com.innospace.platform.iam.infrastructure.authorization.sfs.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.innospace.platform.iam.domain.model.aggregates.User;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@EqualsAndHashCode
+public class UserDetailsImpl implements UserDetails {
+
+    private final String email;
+    @JsonIgnore
+    private final String password;
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
+    private final boolean enabled;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.enabled = true;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        var authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        return new UserDetailsImpl(
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
